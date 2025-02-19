94801d96f41a188a452740d5c0863fb8bb92c88d
ddevin96 <danieledevinco1996@gmail.com>
2025-02-19 13:57:38 +0100

    face

--- src/components/Picture.tsx
+++ src/components/Picture.tsx
@@ -4,7 +4,7 @@ import { Lucide } from "./icons";
 export default () => {
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <Avatar src="/favicon.svg" alt="profile-picture" size="unset" classNames={{ root: "size-2/3" }}>
+      <Avatar src="/face.png" alt="profile-picture" size="unset" classNames={{ root: "size-2/3" }}>
         <Lucide.IconImage className="size-full opacity-10" />
       </Avatar>
     </div>
